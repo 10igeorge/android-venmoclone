55ff9be59e06a6b9564516fecb859623a4b2279f
Izzy George <10izzygeorge@gmail.com>
2016-04-21 14:14:40 -0700

    have fab redirect to new transaction activity

--- app/src/main/java/com/izzy/venmoclone/MainActivity.java
+++ app/src/main/java/com/izzy/venmoclone/MainActivity.java
@@ -1,5 +1,6 @@
 package com.izzy.venmoclone;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,6 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import butterknife.Bind;
@@ -21,6 +23,7 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
         @Bind(R.id.transactionsListView) ListView mTransactionsListView;
+
         private String[] transactions = new String[] {
             "Erica Perez paid Daniel Pinedo",
             "Jean-Marc Pruit paid Matt Cragg",
@@ -53,8 +56,8 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, NewTransactionActivity.class);
+                startActivity(intent);
             }
         });
 
